9941c4106e46996a0adb35c9afeb61378eb14cbe
tocrar <tocrar@gmail.com>
2022-03-03 03:48:48 +0100

    Update colophonetics.js
    ES Module rework as function

--- colophonetics.js
+++ colophonetics.js
@@ -1,104 +1,99 @@
+/* 
+(The MIT License)
 
-String.prototype.colophonetics = (function(){
+Copyright (c) 2011-2013 Jörg Tillmann <colophonetics@joergtillmann.com>
 
-	var substitution = {'ä': 'a', 'ö': 'o', 'ü': 'u', 'ß': 'ss', 'ph': 'f'}
-	var exceptions_leading = {
-		4: ["ca","ch","ck","cl","co","cq","cu","cx"],
-		8: ["dc","ds","dz","tc","ts","tz"]
-	};
-	var exceptions_following = ["sc","zc","cx","kx","qx"];
-	var coding_table = {
-		0: 	"aeijouy",
-		1: 	"bp",
-		2: 	"dt",
-		3: 	"fvw",
-		4: 	"cgkq",
-		48: "x",
-		5: 	"l",
-		6: 	"mn",
-		7: 	"r",
-		8: 	"csz"
-	};
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-	if(Array.prototype.contains === undefined){
-		Array.prototype.contains = function(element){
-			for (var i = 0; i < this.length; ++i) {
-				if (this[i] == element) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-	return function(word){
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-		var word = word || this + "";
+Original: https://github.com/jtillmann/colophoneticjs
 
-		if(typeof word === 'string' && word.indexOf(' ') !== -1){
-			word = word.split(' ');
-		}
+ */
+const substitution = {'ä': 'a', 'ö': 'o', 'ü': 'u', 'ß': 'ss', 'ph': 'f'}
+const exceptions_leading = {
+    4: ["ca","ch","ck","cl","co","cq","cu","cx"],
+    8: ["dc","ds","dz","tc","ts","tz"]
+};
+const exceptions_following = ["sc","zc","cx","kx","qx"];
+const coding_table = {
+    0: 	"aeijouy",
+    1: 	"bp",
+    2: 	"dt",
+    3: 	"fvw",
+    4: 	"cgkq",
+    48: "x",
+    5: 	"l",
+    6: 	"mn",
+    7: 	"r",
+    8: 	"csz"
+};
 
-		if(typeof word === 'object'){
-			var phons = [];
-			for(i in word){
-				phons.push(arguments.callee(word[i]));
-			}
-			return phons.join(' ');
-		}
+export default function colophonetics(word){
 
-		if(typeof word !== 'string'){
-			return '';
-		}
+    if(typeof word === 'string' && word.indexOf(' ') !== -1){
+        word = word.split(' ');
+    }
 
-		word = word.toLowerCase();
-		for(s in substitution){
-			word = word.replace(new RegExp(s, "g"), substitution[s]);
-		}
+    if(typeof word === 'object'){
+        let phons = [];
+        for(let i in word){
+            phons.push(arguments.callee(word[i]));
+        }
+        return phons.join(' ');
+    }
 
-		var value = [];
-		var length = word.length;
+    if(typeof word !== 'string'){
+        return '';
+    }
 
-		for(var i = 0; i < length; i++){
+    word = word.toLowerCase();
+    for(let s in substitution){
+        word = word.replace(new RegExp(s, "g"), substitution[s]);
+    }
 
-			var l 		= word.substr(i, 1);
-			value[i] 	= '';
+    let value = [];
+    let length = word.length;
 
-			if(i === 0 && length > 1 && word[i] + word[i+1] === 'cr'){
-				value[i] = 4;
-			}
+    for(let i = 0; i < length; i++){
 
-			for(code in exceptions_leading){
-				if(exceptions_leading[code].contains(word[i] + word[i+1])){
-					value[i] = parseInt(code);
-				}
-			}
+        let l 		= word.substr(i, 1);
+        value[i] 	= '';
 
-			if(i !== 0 && exceptions_following.contains(word[i-1] + word[i])){
-				value[i] = 8;
-			}
+        if(i === 0 && length > 1 && word[i] + word[i+1] === 'cr'){
+            value[i] = 4;
+        }
 
-			if(value[i] == ''){
-				for(code in coding_table){
-					if(coding_table[code].indexOf(l) !== -1){
-						value[i] = parseInt(code);
-						break;
-					}
-				}
-			}
-		}
+        for(let code in exceptions_leading){
+            if(exceptions_leading[code].includes(word[i] + word[i+1])){
+                value[i] = parseInt(code);
+            }
+        }
 
-		for(var i = 1; i < length; i++){
-			if(value[i] == value[i-1]){
-				value[i] = "";
-			}
-			if(value[i] == 0){
-				value[i] = "";
-			}
-		}
+        if(i !== 0 && exceptions_following.includes(word[i-1] + word[i])){
+            value[i] = 8;
+        }
 
-		return value.join('') + "";
-	}
+        if(value[i] == ''){
+            for(let code in coding_table){
+                if(coding_table[code].indexOf(l) !== -1){
+                    value[i] = parseInt(code);
+                    break;
+                }
+            }
+        }
+    }
 
-	})();
+    for(let i = 1; i < length; i++){
+        if(value[i] == value[i-1]){
+            value[i] = "";
+        }
+        if(value[i] == 0){
+            value[i] = "";
+        }
+    }
 
+    return value.join('') + "";
+}
