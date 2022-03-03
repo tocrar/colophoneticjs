d273dee578ae63158d492c1da24505556d1daa59
tocrar <tocrar@gmail.com>
2022-03-03 11:38:31 +0100

    Update colophonetics.mjs
    formating

--- colophonetics.mjs
+++ colophonetics.mjs
@@ -12,84 +12,87 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 Original: https://github.com/jtillmann/colophoneticjs
 
  */
-const substitution = {'ä': 'a', 'ö': 'o', 'ü': 'u', 'ß': 'ss', 'ph': 'f'}
+const substitution = { ä: "a", ö: "o", ü: "u", ß: "ss", ph: "f" }
 const exceptions_leading = {
-    4: ["ca","ch","ck","cl","co","cq","cu","cx"],
-    8: ["dc","ds","dz","tc","ts","tz"]
-};
-const exceptions_following = ["sc","zc","cx","kx","qx"];
+    4: ["ca", "ch", "ck", "cl", "co", "cq", "cu", "cx"],
+    8: ["dc", "ds", "dz", "tc", "ts", "tz"],
+}
+const exceptions_following = ["sc", "zc", "cx", "kx", "qx"]
 const coding_table = {
-    0: 	"aeijouy",
-    1: 	"bp",
-    2: 	"dt",
-    3: 	"fvw",
-    4: 	"cgkq",
+    0: "aeijouy",
+    1: "bp",
+    2: "dt",
+    3: "fvw",
+    4: "cgkq",
     48: "x",
-    5: 	"l",
-    6: 	"mn",
-    7: 	"r",
-    8: 	"csz"
-};
+    5: "l",
+    6: "mn",
+    7: "r",
+    8: "csz",
+}
 
-export default function colophonetics(word){
-        
-    if(typeof word === 'string' && word.trim().includes(' ')){
-        word = word.split(' ');
+export default function colophonetics(word) {
+    if (typeof word === "string" && word.trim().includes(" ")) {
+        word = word.split(" ")
     }
 
-    if(typeof word === 'object'){
-        return Array.from(word).map(words => {return colophonetics(words)}).join(' ')
+    if (typeof word === "object") {
+        return Array.from(word)
+            .map((words) => {
+                return colophonetics(words)
+            })
+            .join(" ")
     }
 
-    if(typeof word !== 'string'){
-        return '';
+    if (typeof word !== "string") {
+        return ""
     }
 
-    word = word.toLowerCase();
-    for(let s in substitution){
-        word = word.replace(new RegExp(s, "g"), substitution[s]);
+    word = word.trim().toLowerCase()
+    for (let s in substitution) {
+        // word = word.replace(new RegExp(s, "g"), substitution[s])
+        word = word.replace(s, substitution[s])
     }
 
-    let value = [];
-    let length = word.length;
+    const value = []
+    const length = word.length
 
-    for(let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
+        let l = word.substr(i, 1)
+        value[i] = ""
 
-        let l 		= word.substr(i, 1);
-        value[i] 	= '';
-
-        if(i === 0 && length > 1 && word[i] + word[i+1] === 'cr'){
-            value[i] = 4;
+        if (i === 0 && length > 1 && word[i] + word[i + 1] === "cr") {
+            value[i] = 4
         }
 
-        for(let code in exceptions_leading){
-            if(exceptions_leading[code].includes(word[i] + word[i+1])){
-                value[i] = parseInt(code);
+        for (let code in exceptions_leading) {
+            if (exceptions_leading[code].includes(word[i] + word[i + 1])) {
+                value[i] = parseInt(code)
             }
         }
 
-        if(i !== 0 && exceptions_following.includes(word[i-1] + word[i])){
-            value[i] = 8;
+        if (i !== 0 && exceptions_following.includes(word[i - 1] + word[i])) {
+            value[i] = 8
         }
 
-        if(value[i] == ''){
-            for(let code in coding_table){
-                if(coding_table[code].indexOf(l) !== -1){
-                    value[i] = parseInt(code);
-                    break;
+        if (value[i] == "") {
+            for (let code in coding_table) {
+                if (coding_table[code].indexOf(l) !== -1) {
+                    value[i] = parseInt(code)
+                    break
                 }
             }
         }
     }
 
-    for(let i = 1; i < length; i++){
-        if(value[i] == value[i-1]){
-            value[i] = "";
+    for (let i = 1; i < length; i++) {
+        if (value[i] == value[i - 1]) {
+            value[i] = ""
         }
-        if(value[i] == 0){
-            value[i] = "";
+        if (value[i] == 0) {
+            value[i] = ""
         }
     }
 
-    return value.join('') + "";
+    return value.join("") + ""
 }
